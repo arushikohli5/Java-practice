ab3526f921e19092d6f569b701548a14689a314b
Arushi <arushi1467.cse18@chitkara.edu.in>
2021-02-09 22:59:58 +0530

    Create 4sum.java

---
+++ 4sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        int count=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        
+        int n=A.length;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int sum=A[i]+B[j];
+                map.put(sum,map.getOrDefault(sum,0)+1);
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int sum=C[i]+D[j];
+                count +=map.getOrDefault(-sum,0);
+            }
+        }
+        return count;
+    }
+}
